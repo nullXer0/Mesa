2e0f790721530b9054f09f66fdaf90776bb6d4c6
null_Xer0 <nullZer0@crimsonvalkyrie.com>
2023-06-25 22:28:32 -0400

    Fix login streaks not working

--- src/main/java/com/crimsonvalkyrie/mesa/listeners/JoinListener.java
+++ src/main/java/com/crimsonvalkyrie/mesa/listeners/JoinListener.java
@@ -20,7 +20,7 @@ public class JoinListener implements Listener
 
 	private static final String QUERY_STATEMENT = "SELECT * FROM " + Database.DAYS_PLAYED + " WHERE uuid = ?;";
 	private static final String EXIST_STATEMENT = "SELECT EXISTS(SELECT * FROM " + Database.DAYS_PLAYED + " WHERE uuid = ?);";
-	private static final String INSERT_STATEMENT = "INSERT INTO " + Database.DAYS_PLAYED + "(uuid, total_days, last_played, current_streak, longest_streak) VALUES(?, ?, ?);";
+	private static final String INSERT_STATEMENT = "INSERT INTO " + Database.DAYS_PLAYED + "(uuid, last_played, total_days, current_streak, longest_streak) VALUES(?, ?, ?, ?, ?);";
 	private static final String UPDATE_STATEMENT = "UPDATE " + Database.DAYS_PLAYED + " SET last_played = ?, total_days = ?, current_streak = ?, longest_streak = ? WHERE player = ?";
 
 	@EventHandler(priority = EventPriority.MONITOR)
@@ -46,15 +46,25 @@ public class JoinListener implements Listener
 			if(newPlayer)
 			{
 				PreparedStatement insertStatement = conn.prepareStatement(INSERT_STATEMENT);
+				//uuid
 				insertStatement.setString(1, uuid.toString());
+				//last_played
 				insertStatement.setDate(2, Date.valueOf(LocalDate.now()));
+				//total_days
 				insertStatement.setInt(3, 1);
+				//current_streak
+				insertStatement.setInt(4, 1);
+				//longest_streak
+				insertStatement.setInt(5, 1);
+
 				insertStatement.execute();
 			}
 			else
 			{
 				int daysPlayed;
 				Date last_played;
+				int current_streak;
+				int longest_streak;
 
 				PreparedStatement queryStatement = conn.prepareStatement(QUERY_STATEMENT);
 				queryStatement.setString(1, uuid.toString());
@@ -62,13 +72,30 @@ public class JoinListener implements Listener
 				queryResult.next();
 				last_played = queryResult.getDate("last_played");
 				daysPlayed = queryResult.getInt("total_days");
+				current_streak = queryResult.getInt("current_streak");
+				longest_streak = queryResult.getInt("longest_streak");
 
 				if(last_played.compareTo(Date.valueOf(LocalDate.now())) != 0)
 				{
 					PreparedStatement updateStatement = conn.prepareStatement(UPDATE_STATEMENT);
-					updateStatement.setInt(1, daysPlayed + 1);
-					updateStatement.setDate(2, Date.valueOf(LocalDate.now()));
-					updateStatement.setString(3, uuid.toString());
+					//last_played
+					updateStatement.setDate(1, Date.valueOf(LocalDate.now()));
+					//total_days
+					updateStatement.setInt(2, daysPlayed + 1);
+					//current_streak
+					if(last_played.compareTo(Date.valueOf(LocalDate.now().minusDays(1))) == 0)
+					{
+						updateStatement.setInt(3, current_streak + 1);
+					}
+					else
+					{
+						updateStatement.setInt(3, 1);
+					}
+					//longest_streak
+					updateStatement.setInt(4, Math.max(current_streak, longest_streak));
+					//uuid
+					updateStatement.setString(5, uuid.toString());
+
 					updateStatement.execute();
 				}
 			}
